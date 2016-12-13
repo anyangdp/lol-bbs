45b05ba0e83fffb3902d1ec9e514c0a3b9ee823c
anyangdp <anyangdp@aliyun.com>
2016-12-13 23:43:56 +0800

    sss

--- src/main/java/com/lolbbs/bootStrap/Application.java
+++ src/main/java/com/lolbbs/bootStrap/Application.java
@@ -1,11 +1,17 @@
 package com.lolbbs.bootStrap;
 
+import javafx.scene.Parent;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-/*@Configuration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})*/
+//@Configuration
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(value = {"com.lolbbs.controller"})
 @SpringBootApplication//等于上面三个配置
 public class Application {
@@ -13,7 +19,17 @@ public class Application {
 		/**
 		 * 设置热部署是否生效
 		 */
-		System.setProperty("spring.devtools.restart.enabled", "false");
-		SpringApplication.run(Application.class, args);
+//		System.setProperty("spring.devtools.restart.enabled", "false");
+		/*SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);*///第一种
+
+		new SpringApplicationBuilder()
+				.sources(Parent.class)
+				.child(Application.class)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
+
 	}
 }
--- src/main/java/com/lolbbs/controller/GreetingController.java
+++ src/main/java/com/lolbbs/controller/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 	private static final String template = "Hello,sss %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/hello")
+    @RequestMapping("/hellos")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
