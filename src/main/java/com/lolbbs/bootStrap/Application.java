116d7a94dea6bb1f86a0c30305644aff108ec581
anyangdp <anyangdp@aliyun.com>
2017-01-16 17:33:18 +0800

    add restapi

--- src/main/java/com/lolbbs/bootStrap/Application.java
+++ src/main/java/com/lolbbs/bootStrap/Application.java
@@ -1,21 +1,20 @@
 package com.lolbbs.bootStrap;
 
-import javafx.scene.Parent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javafx.scene.Parent;
 
 //@Configuration
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(value = {"com.lolbbs.controller"})
 @SpringBootApplication//等于上面三个配置
 public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 		/**
 		 * 设置热部署是否生效
@@ -33,4 +32,5 @@ public class Application {
 				.run(args);
 
 	}
+
 }
